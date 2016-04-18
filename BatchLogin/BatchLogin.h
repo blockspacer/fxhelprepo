96cf25fb7613433323d7ca66365c1f55695d8da0
MichaelTan <2335977628@qq.com>
2016-04-19 00:32:21 +0800

    增加批量登录工程

---
+++ BatchLogin/BatchLogin.h
@@ -0,0 +1,32 @@
+
+// BatchLogin.h : PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CBatchLoginApp: 
+// 有关此类的实现，请参阅 BatchLogin.cpp
+//
+
+class CBatchLoginApp : public CWinApp
+{
+public:
+	CBatchLoginApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CBatchLoginApp theApp;
