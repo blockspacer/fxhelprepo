0e7bb19242f8e77e47956afaf2d78374f13a3c71
MichaelTan <2335977628@qq.com>
2016-05-10 01:25:23 +0800

    测试RSA加密算法成功

--- TestProject/TestProject.cpp
+++ TestProject/TestProject.cpp
@@ -2,6 +2,9 @@
 //
 
 #include "stdafx.h"
+#include <fstream>
+#include <iostream>
+#include <sstream>
 #include <assert.h>
 #include "Network/User.h"
 #include "Network/CurlWrapper.h"
@@ -11,7 +14,16 @@
 #include "third_party/chromium/base/command_line.h"
 #include "third_party/chromium/base/at_exit.h"
 
+#include "third_party/cryptopp/cryptlib.h"
+#include "third_party/cryptopp/rsa.h"
+#include "third_party/cryptopp/aes.h"
+#include "third_party/cryptopp/modes.h"
+#include "third_party/cryptopp/filters.h"
+#include "third_party/cryptopp/files.h"
+#include "third_party/cryptopp/hex.h"
+#include "third_party/cryptopp/randpool.h"
 
+using namespace CryptoPP;
 
 void SingleUserSingleRoomTest()
 {
@@ -63,14 +75,86 @@ void InitAppLog()
     logging::SetLogItems(false, true, true, true);
 }
 
+static CryptoPP::OFB_Mode<CryptoPP::AES>::Encryption s_globalRNG;
+
+RandomNumberGenerator & GlobalRNG()
+{
+    return s_globalRNG;
+}
+
+std::string RSAEncryptString(std::istream* pubFilename, const std::string& seed, const std::string& message)
+{
+    FileSource pubFile(*pubFilename, true, new HexDecoder);
+    RSAES_OAEP_SHA_Encryptor pub(pubFile);
+
+    RandomPool randPool;
+    randPool.IncorporateEntropy((byte *)seed.c_str(), seed.length());
+
+    std::string result;
+    StringSource(message, true, new PK_EncryptorFilter(randPool, pub, new HexEncoder(new StringSink(result))));
+    return result;
+}
+
+std::string RSADecryptString(std::istream* privFilename, const std::string& ciphertext)
+{
+    FileSource privFile(*privFilename, true, new HexDecoder);
+    RSAES_OAEP_SHA_Decryptor priv(privFile);
+
+    std::string result;
+    StringSource(ciphertext, true, new HexDecoder(new PK_DecryptorFilter(GlobalRNG(), priv, new StringSink(result))));
+    return result;
+}
+
+void GenerateRSAKey(unsigned int keyLength, std::ostream* privFilename, std::ostream* pubFilename, const std::string& seed)
+{
+    RandomPool randPool;
+    randPool.IncorporateEntropy((byte *)seed.c_str(), seed.length());
+
+    RSAES_OAEP_SHA_Decryptor priv(randPool, keyLength);
+    HexEncoder privFile(new FileSink(*privFilename));
+    priv.DEREncode(privFile);
+    privFile.MessageEnd();
+
+    RSAES_OAEP_SHA_Encryptor pub(priv);
+    HexEncoder pubFile(new FileSink(*pubFilename));
+    pub.DEREncode(pubFile);
+    pubFile.MessageEnd();
+}
+
+void RSATest()
+{   
+    std::string seed = CryptoPP::IntToString(time(NULL));
+    seed.resize(16);
+    s_globalRNG.SetKeyWithIV((byte *)seed.data(), 16, (byte *)seed.data());
+    std::stringbuf publickey;
+    std::stringbuf privatekey;
+    std::ostream oss_publickey(&publickey);
+    std::ostream oss_privatekey(&privatekey);
+    GenerateRSAKey(512, &oss_privatekey, &oss_publickey, seed);
+
+    std::istream iss_publickey(&publickey);
+    std::istream iss_privatekey(&privatekey);
+
+    //std::string publicfile = "e:/public.txt";
+    //std::string privatefile = "e:/private.txt";
+    //GenerateRSAKey(512, privatefile, publicfile, seed);
+
+
+    std::string message = "123456";
+   
+    std::string ciphertext = RSAEncryptString(&iss_publickey, seed, message);
+    std::string decrypted = RSADecryptString(&iss_privatekey, ciphertext.c_str());
+}
+
 int _tmain(int argc, _TCHAR* argv[])
 {
     base::AtExitManager atExitManager;
-    CurlWrapper::CurlInit();
-    //SingleUserSingleRoomTest();
-    MultiUserMultiRoomTest();
+    RSATest();
+    //CurlWrapper::CurlInit();
+    ////SingleUserSingleRoomTest();
+    //MultiUserMultiRoomTest();
 
-    CurlWrapper::CurlCleanup();
+    //CurlWrapper::CurlCleanup();
 	return 0;
 }
 
