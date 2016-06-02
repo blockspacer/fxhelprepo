12afe9f51aa4717f6e95b69eec26e435e7539852
MichaelTan <2335977628@qq.com>
2016-06-03 00:32:59 +0800

    增加ip代理类，相关代理功能未实现

---
+++ Network/IpProxy.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <string>
+#include "third_party/chromium/base/basictypes.h"
+
+class IpProxy
+{
+public:
+    enum class PROXY_TYPE
+    {
+        PROXY_TYPE_NONE = 0,
+        PROXY_TYPE_HTTP = 1,
+        PROXY_TYPE_SOCKS4 = 2,
+        PROXY_TYPE_SOCKS4A = 3,
+        PROXY_TYPE_SOCKS5 = 4
+    };
+
+    IpProxy();
+    ~IpProxy();
+
+    void SetProxyType(PROXY_TYPE proxytype);
+    PROXY_TYPE GetProxyType() const;
+
+    void SetProxyIp(const std::string& proxyip);
+    std::string GetProxyIp() const;
+
+    void SetProxyPort(uint16 proxyport);
+    uint16 GetProxyPort() const;
+
+private:
+    PROXY_TYPE proxytype_;
+    std::string proxyip_;
+    uint16 proxyport_;
+};
+
