0d634c699f72b5f7c860c4a86826884749065732
michaelforfxhelp <developpermichael@gmail.com>
2016-06-15 18:57:43 +0800

    增加充值代理管理新工程

---
+++ RechargeAgent/RechargeAgentDlg.h
@@ -0,0 +1,31 @@
+
+// RechargeAgentDlg.h : 头文件
+//
+
+#pragma once
+
+
+// CRechargeAgentDlg 对话框
+class CRechargeAgentDlg : public CDialogEx
+{
+// 构造
+public:
+	CRechargeAgentDlg(CWnd* pParent = NULL);	// 标准构造函数
+
+// 对话框数据
+	enum { IDD = IDD_RECHARGEAGENT_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
