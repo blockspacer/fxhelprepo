e49e971526aaece45730cec46c7385f6bd604412
MichaelTan <2335977628@qq.com>
2016-09-04 20:46:14 +0800

    增加膜拜列表操作功能

--- AntiFlood/WorshipDlg.h
+++ AntiFlood/WorshipDlg.h
@@ -22,22 +22,24 @@ public:
 // 对话框数据
 	enum { IDD = IDD_DLG_WORSHIP };
 
+    afx_msg void OnBnClickedBtnWorship();
+    afx_msg void OnBnClickedBtnAddToList();
+    afx_msg void OnBnClickedBtnWorshipSelect();
+    afx_msg void OnBnClickedBtnDeleteSelect();
+
 protected:
 	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
 
 	DECLARE_MESSAGE_MAP()
-public:
+
+private:
     CEdit m_edit_fanxing_id;
     CEdit m_edit_roomid;
     CDateTimeCtrl m_time_worship;
     CButton m_check_auto_worship;
     CListCtrl m_list_worship;
-    afx_msg void OnBnClickedBtnWorship();
-    afx_msg void OnBnClickedBtnAddToList();
-    afx_msg void OnBnClickedBtnWorshipSelect();
 
-private:
     typedef std::pair<std::wstring, std::wstring> worship_pair;
     bool GetSelectItems(std::vector<worship_pair>* select_items);
-    NetworkHelper* network_helper_;    
+    NetworkHelper* network_helper_;
 };
