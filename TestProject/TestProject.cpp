2f298bc8ae2191c92df28ac830662c95e37c07c9
michaelforfxhelp <2335977628@qq.com>
2016-11-06 17:17:00 +0800

    测试打包功能

--- TestProject/TestProject.cpp
+++ TestProject/TestProject.cpp
@@ -12,6 +12,7 @@
 #include "Network/Network.h"
 #include "third_party/chromium/base/at_exit.h"
 #include "third_party/chromium/base/run_loop.h"
+#include "third_party/zlib/zlib.h"
 
 bool MakePostdata(const std::map<std::string, std::string>& postmap,
     std::vector<uint8>* postdata)
@@ -51,13 +52,53 @@ void DoRobotTest()
 
 }
 
+int ZLibTest()
+{
+    char text[] = "zlib compress and uncompress test\nturingo@163.com\n2012-11-05\n";
+    uLong tlen = strlen(text) + 1;	/* 需要把字符串的结束符'\0'也一并处理 */
+    char* buf = NULL;
+    uLong blen;
+
+    /* 计算缓冲区大小，并为其分配内存 */
+    blen = compressBound(tlen);	/* 压缩后的长度是不会超过blen的 */
+    if ((buf = (char*)malloc(sizeof(char) * blen)) == NULL)
+    {
+        printf("no enough memory!\n");
+        return -1;
+    }
+
+    /* 压缩 */
+    if (compress((unsigned char*)buf, &blen, (unsigned char*)text, tlen) != Z_OK)
+    {
+        printf("compress failed!\n");
+        return -1;
+    }
+
+    /* 解压缩 */
+    if (uncompress((unsigned char*)text, &tlen, (unsigned char*)buf, blen) != Z_OK)
+    {
+        printf("uncompress failed!\n");
+        return -1;
+    }
+
+    /* 打印结果，并释放内存 */
+    printf("%s", text);
+    if (buf != NULL)
+    {
+        free(buf);
+        buf = NULL;
+    }
+    return 0;
+}
+
+
 int _tmain(int argc, _TCHAR* argv[])
 {   
     base::AtExitManager atExitManager;
 
     NetworkInitialize();
 
-    DoRobotTest();
+    ZLibTest();
 
     NetworkFainalize();
 	return 0;
