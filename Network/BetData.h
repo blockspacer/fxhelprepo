827fb1760c15d82a59c8ae85acf7b3b5d79325a2
michaelforfxhelp <2335977628@qq.com>
2016-12-13 22:56:21 +0800

    补充漏掉的文件

---
+++ Network/BetData.h
@@ -0,0 +1,16 @@
+#include "third_party/chromium/base/basictypes.h"
+
+struct BetShowData
+{
+    uint32 bet_gid;
+    uint32 odds;
+    //uint32 count;
+};
+
+struct BetResult
+{
+    uint32 time = 0;
+    uint32 result = 0;
+    uint32 display_result = 0;
+    uint32 random = 0;
+};
