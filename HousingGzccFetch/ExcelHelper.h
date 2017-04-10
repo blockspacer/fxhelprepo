db2130fb32f0207af0ea0ac9771bdc92838b927a
michaelforfxhelp <developpermichael@gmail.com>
2017-04-10 12:18:46 +0800

    增加Excel处理文件

---
+++ HousingGzccFetch/ExcelHelper.h
@@ -0,0 +1,47 @@
+#pragma once
+#include <string>
+#include <vector>
+#include "third_party/chromium/base/basictypes.h"
+
+#include "HousingGzccFetch//Application.h"
+#include "HousingGzccFetch/Range.h"
+#include "HousingGzccFetch/Workbook.h"
+#include "HousingGzccFetch/Workbooks.h"
+#include "HousingGzccFetch/Worksheet.h"
+#include "HousingGzccFetch/Worksheets.h"
+
+class FamilyDataModle
+{
+public:
+    FamilyDataModle();
+    ~FamilyDataModle();
+};
+
+
+class ExcelHelper
+{
+public:
+    ExcelHelper();
+    ~ExcelHelper();
+
+    bool Init(const std::wstring& filepath);
+
+    bool Create(const std::wstring& filepath);
+    bool Open(const std::wstring& filepath);
+    bool Close();
+
+    bool Export(std::vector<std::vector<std::wstring>> data);
+
+private:
+
+    bool OpenSheet(const std::wstring& sheetname, CWorksheet* sheet);
+    bool SetString(uint32 rowid, uint32 columnid, const std::wstring& data);
+
+    std::wstring templatepath_;
+    CApplication app_;
+    CWorkbook book_;
+    CWorkbooks books_;
+    CWorksheets sheets_;
+    CWorksheet sheet_;
+};
+
