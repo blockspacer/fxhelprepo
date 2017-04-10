e7f3c18e616fba347763c93eef986a883d48eb4a
michaelforfxhelp <23359777628@qq.com>
2017-04-10 08:24:32 +0800

    增加数据采集界面工程

---
+++ HousingGzccFetch/HousingGzccFetch.h
@@ -0,0 +1,32 @@
+
+// HousingGzccFetch.h : PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CHousingGzccFetchApp: 
+// 有关此类的实现，请参阅 HousingGzccFetch.cpp
+//
+
+class CHousingGzccFetchApp : public CWinApp
+{
+public:
+	CHousingGzccFetchApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CHousingGzccFetchApp theApp;
