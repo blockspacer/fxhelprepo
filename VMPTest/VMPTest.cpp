2cffd88045b57a8cf0a6f4f9de095ab1d7ecc9ab
MichaelTan <micahelforfxhelper>
2017-12-20 23:43:26 +0800

    增加可以通过的授权功能

--- VMPTest/VMPTest.cpp
+++ VMPTest/VMPTest.cpp
@@ -3,12 +3,8 @@
 
 #include "stdafx.h"
 #include <windows.h>
+#include <stdio.h>
 #include "VMProtect/VMProtectSDK.h"
-
-//int _tmain(int argc, _TCHAR* argv[])
-//{
-//	return 0;
-//}
 
 #define PRINT_HELPER(state, flag) if (state & flag) printf("%s ", #flag)
 void print_state(INT state)
@@ -30,30 +26,41 @@ void print_state(INT state)
     printf("\n");
 }
 
+char *read_serial(const char *fname)
+{
+    FILE *f;
+    if (0 != fopen_s(&f, fname, "rb")) return NULL;
+    fseek(f, 0, SEEK_END);
+    int s = ftell(f);
+    fseek(f, 0, SEEK_SET);
+    char *buf = new char[s + 1];
+    fread(buf, s, 1, f);
+    buf[s] = 0;
+    fclose(f);
+    return buf;
+}
+
+// The foo() method is very short, but we need it to be an individual function
+// so we asked the compiler to not compile it inline
+__declspec(noinline) void foo()
+{
+    printf("I'm foo!\n");
+}
+
 int main(int argc, char **argv)
 {
-    char *serial = R"(hsxtk/xQRqrCMaupHWLSlgKKuln8es2L12VsQNKwduQ6NZg76TYHT7tPQhFBd8SDtJNHR6m3x9mfb1nsJG1btjpvHHbIWuDXnQzxex5VPwzqKh0bFAAfQOB1ONbI8T6fGTpuVC8N/CK8uAQ+O1O7fCjWEWresJQZrju7bi5u+PnU87oKTdGKwrgz1L0BEn+z4bSpbNc9mgOVTOoyTUaeDwblhS0zi3ZtE3sufzolIEXxEn4vMF9FsbI0hxfqobXN534ij5XW0IbZTS9Ciu878tS+mMoLGhDwZxhQ2MXoHDlt46AXEG3NZ2xfE4wk8p0OuOO7HmhcemvVPVcTYunn5A==)";
-
+    char *serial = read_serial("d:/serial.ini");
     int res = VMProtectSetSerialNumber(serial);
-    print_state(res);
-
+    delete[] serial;
     if (res)
     {
-        VMProtectSerialNumberData sd = { 0 };
-        VMProtectGetSerialNumberData(&sd, sizeof(sd));
-        printf("max. build date: y = %d, m = %d, d = %d\n", sd.dtMaxBuild.wYear, sd.dtMaxBuild.bMonth, sd.dtMaxBuild.bDay);
-        printf("please register!\n");
-        //return 0;
+        printf("serial number is bad\n");
+        print_state(res);
+        return 0;
     }
-
-    printf("I'm registered\n");
-
-    int nSize = VMProtectGetCurrentHWID(NULL, 0);
-    char *buf = new char[nSize];
-    VMProtectGetCurrentHWID(buf, nSize);
-    printf("HWID: %s\n", buf);
-    delete[] buf;
-
+    printf("serial number is correct, calling foo()\n");
+    foo();
+    printf("done\n");
     return 0;
 }
 
